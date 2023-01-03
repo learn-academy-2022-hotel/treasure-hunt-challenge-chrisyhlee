999bc14b5fec22e29747876646b6e285f3ab011b
chrisyhlee <leechr8393@gmail.com>
2023-01-03 11:29:23 -0800

    user 4

--- src/App.js
+++ src/App.js
@@ -15,11 +15,10 @@ const App = () => {
     "?"
   ])
 
-  const handleGamePlay = (clickedSquare) => {
-    let updateBoard = [...board]
-    updateBoard[clickedSquare] = "🌴"
-    setBoard(updateBoard)
-  }
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState()
+
+console.log(treasureLocation);
 
   const reset = () => {
     setBoard([
@@ -33,14 +32,31 @@ const App = () => {
       "?",
       "?"
     ])
+    setTreasureLocation(Math.floor(Math.random() * board.length))
   }
+
+  const handleGamePlay = (clickedSquare) => {
+    let updateBoard = [...board]
+    treasureLocation === 0 ? setBombLocation(treasureLocation + 1) : setBombLocation(treasureLocation - 1)
+    if (clickedSquare === treasureLocation) {
+      updateBoard[clickedSquare] = "🎁"
+      setBoard(updateBoard)
+    } else if (clickedSquare === bombLocation) {
+      updateBoard[clickedSquare] = "💣"
+      setBoard(updateBoard)
+    } else {
+      updateBoard[clickedSquare] = "🌴"
+      setBoard(updateBoard)
+    }
+  }
+
   
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className='resetButton'>
-        <button onClick={reset}>Reset</button>
+        <button onClick={reset}>Play Again</button>
       </div>
       <div className="gameBoard">
         {board.map((square, i) => {
